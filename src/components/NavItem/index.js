3a1203df8a5462ffd2c5ceb087826cb13b22f41d
Gai Isay <gaiisay@outlook.de>
2022-10-20 09:17:49 +0200

    change navitem if

--- src/components/NavItem/index.js
+++ src/components/NavItem/index.js
@@ -1,23 +1,17 @@
 import "./NavItem.css";
 
 function NavItem({ children, active }) {
-  if (active) {
-    return (
-      <li className="navigation__list-item navigation__list-item--active">
-        <a className="navigation__link" href="/index.html" aria-label="go to home page">
-          {children}
-        </a>
-      </li>
-    );
-  } else {
-    return (
-      <li className="navigation__list-item">
-        <a className="navigation__link" href="/index.html" aria-label="go to home page">
-          {children}
-        </a>
-      </li>
-    );
-  }
+  return (
+    <li
+      className={
+        active ? "navigation__list-item navigation__list-item--active" : "navigation__list-item"
+      }
+    >
+      <a className="navigation__link" href="/index.html" aria-label="go to home page">
+        {children}
+      </a>
+    </li>
+  );
 }
 
 export default NavItem;
